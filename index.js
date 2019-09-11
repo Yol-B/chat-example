595872d1c80f26db03f9ae20756eddc8b39906d7
camaymaryann123@gmail.com <camaymaryann@123@gmail.com>
2019-09-11 20:56:57 +0800

    added private chat

--- index.js
+++ index.js
@@ -2,25 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-var users = []
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
-    if (msg.receiver != "") {
-      socket.broadcast.emit(msg.receiver, msg.message);
-      console.log("Receiver : "+msg.receiver + " : "+typeof msg.receiver)
-      console.log("Message : "+msg.message+ " : "+typeof msg.message)
-      console.log("Sender : "+msg.username + " : "+typeof msg.username)
-    } else {
-      socket.broadcast.emit('chat message', msg);
-
-    }
+    socket.broadcast.emit("chat message", {receiver : msg.receiver , message : msg.message , sender : msg.sender});
   });
-  // console.log(users)
-
 });
 
 
