6c692c6db795d1ed7db24b3e96951c15d1684b11
Yol-B <yoltorres24@gmail.com>
2019-09-11 16:37:15 +0800

    private chat on process Laban Lang!

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
-app.get('/', function(req, res){
+var users = []
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    if (msg.receiver != "") {
+      socket.broadcast.emit(msg.receiver, msg.message);
+      console.log("Receiver : "+msg.receiver + " : "+typeof msg.receiver)
+      console.log("Message : "+msg.message+ " : "+typeof msg.message)
+      console.log("Sender : "+msg.username + " : "+typeof msg.username)
+    } else {
+      socket.broadcast.emit('chat message', msg);
+
+    }
   });
+  // console.log(users)
+
 });
 
-http.listen(port, function(){
+
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
