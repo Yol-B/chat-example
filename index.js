c7e1f9a1555fbd5223c619a384b1d8e325d2e80f
Yol-B <yoltorres24@gmail.com>
2019-09-12 11:05:49 +0800

    added online counter

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+
+
 io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    socket.broadcast.emit("chat message", {receiver : msg.receiver , message : msg.message , sender : msg.sender});
+  socket.on('set-online', function (user) {
+    console.log(user.username + " is connected!")
+  })
+  socket.on('message', function (msg) {
+    socket.broadcast.emit("message", { receiver: msg.receiver, message: msg.message, sender: msg.sender });
   });
+  setInterval(function () {
+    socket.emit("online-count", { online: socket.client.conn.server.clientsCount })
+  }, 50)
 });
+
 
 
 http.listen(port, function () {
