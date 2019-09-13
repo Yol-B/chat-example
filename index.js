3a7776691e595f0f89cc8609ef8ec26a7b80ef21
Leonilo Torres <torresle_sd2053@ceaccs.net>
2019-09-13 16:45:24 +0800

    ambot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,12 +13,18 @@ io.on('connection', function (socket) {
   socket.on('set-online', function (user) {
     console.log(user.username + " is connected!")
   })
+  console.log(socket.client.conn);
+
   socket.on('message', function (msg) {
     socket.broadcast.emit("message", { receiver: msg.receiver, message: msg.message, sender: msg.sender });
   });
   setInterval(function () {
     socket.emit("online-count", { online: socket.client.conn.server.clientsCount })
   }, 50)
+
+  socket.on('disconnect',function(e){
+    console.log("offline : "+socket.id);
+  })
 });
 
 
