88627f9d0cf8ca098bcb9797479a3d2ae938b9d7
Yol-B <yoltorres24@gmail.com>
2019-09-14 18:55:55 +0800

    added online list

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   socket.on('set-online', function (user) {
     activeUsers.push({ id: socket.id, username: user.username })
+
   })
   setInterval(function () {
-    socket.emit("online", { online: activeUsers })
-  },50)
+    socket.emit("online", activeUsers)
+  },1000)
 
   socket.on('message', function (msg) {
     socket.broadcast.emit("message", { receiver: msg.receiver, message: msg.message, sender: msg.sender });
   });
-
-
 
   socket.on('disconnect', function (e) {
     for (let i = 0; i < activeUsers.length; i++) {
       if (activeUsers[i].id == socket.id) {
         activeUsers.splice(i, 1);
       }
+
     }
 
   })
