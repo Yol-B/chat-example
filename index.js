7b917876dd3819f1372787f3f309957cd5566b00
Yol-B <yoltorres24@gmail.com>
2019-09-15 18:11:00 +0800

    user join and user leave

--- index.js
+++ index.js
@@ -10,11 +10,38 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   socket.on('set-online', function (user) {
     activeUsers.push({ id: socket.id, username: user.username })
+    socket.broadcast.emit('joined', user.username)
+    socket.emit("online", activeUsers)
+
+    // setInterval(function () {
+    //   socket.emit("online", activeUsers)
+    // }, 1000)
 
   })
-  setInterval(function () {
-    socket.emit("online", activeUsers)
-  },1000)
+
+  socket.on('typing', function (isTyping) {
+    socket.broadcast.emit('typing', isTyping)
+  })
+
+  socket.on('stop-typing', function (stop_typing) {
+    socket.broadcast.emit('stop-typing', stop_typing)
+  })
+
+  socket.on('check-username', function (username) {
+    var found = false;
+    for (let i = 0; i < activeUsers.length; i++) {
+      if (activeUsers[i].username == username) {
+        found = true;
+        socket.emit('verify-username', '0');
+        break;
+      }
+    }
+    if (!found) {
+      socket.emit('verify-username', '1');
+    }
+  })
+
+
 
   socket.on('message', function (msg) {
     socket.broadcast.emit("message", { receiver: msg.receiver, message: msg.message, sender: msg.sender });
@@ -23,13 +50,15 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function (e) {
     for (let i = 0; i < activeUsers.length; i++) {
       if (activeUsers[i].id == socket.id) {
+        socket.broadcast.emit('leave', activeUsers[i].username)
+        socket.emit("online", activeUsers)
         activeUsers.splice(i, 1);
+        break;
       }
 
     }
 
   })
-
 });
 
 http.listen(port, function () {
